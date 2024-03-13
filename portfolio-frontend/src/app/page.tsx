4e96859e45f88aa5e016c4f702b50523f343bdc3
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-03-13 23:28:25 +0530

    portfolio init

---
+++ portfolio-frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Header } from "@/components/Header";
+
+export default function(){
+  return <>
+  <main className="flex flex-col gap-5 p-5 justify-center md:w-2/3 w-full mx-auto bg-black">
+    <Header/>
+  </main>
+  </>
+}
---
+++ portfolio-frontend/src/components/Header.tsx
@@ -0,0 +1,5 @@
+export function Header(){
+    return <>
+    <h1 className="text-white text-5xl font-bold">I'm a software engineer that rarely writes code.</h1>
+    </>
+}
