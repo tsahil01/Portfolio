96baeebbde41bb9ca3754bb12a93a283fac2a401
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-03-28 11:36:57 +0530

    add vercel analytics

--- portfolio-frontend/src/app/layout.tsx
+++ portfolio-frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Chat } from "@/components/Chat";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
