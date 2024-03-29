2e704b4a74574a3b85181df31ca77ed33f7590bf
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2024-03-29 09:39:20 +0530

    text-xs on header

--- portfolio-frontend/src/components/Header.tsx
+++ portfolio-frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ export function Header(){
     <h1 className="text-white md:text-5xl text-4xl font-bold mx-auto lg:w-3/5 w-full">
     Daily coding as a <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-indigo-400">Developer</span>, always pushing <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-blue-400">Code.</span>
     </h1>
-    <p className="text-zinc-400 mx-auto lg:w-3/5 w-full text-sm md:text-base mt-8">
+    <p className="text-zinc-400 mx-auto lg:w-3/5 w-full text-xs md:text-base mt-8">
     Hi, I&apos;m Sahil, a passionate developer from India. <br/> 
     I love coding, and I&apos;m really into finding smart solutions to problems. Currently, I&apos;m exploring Web3.
     Oh, and I&apos;m also a big fan of caffeine; it keeps me going when I&apos;m deep into coding. Let&apos;s make some awesome stuff together!
