b6ac685ae770546323906c4f4c35105947fd63df
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-07-30 10:22:44 +0530

    chore: Update projectsAtom with new project details

--- portfolio-frontend/src/atoms/projectsAtom.ts
+++ portfolio-frontend/src/atoms/projectsAtom.ts
@@ -1,31 +1,46 @@
 import { atom } from "recoil";
 
 export const projectsAtom = atom({
-    key: "projectsAtom",
-    default: [
-      {
-        projectName: "QuickShare: Effortlessly Share Your Files Anywhere",
-        projectDescription: "Simplify your file sharing experience with our intuitive platform. Seamlessly upload, share, and access your files anytime, anywhere. Start sharing smarter today!",
-        projectImgUrl: "/quickShare.png",
-        projectUrl: "https://ezfiles.vercel.app/"
-      },
-        {
-          projectName: "Eznote: Markdown Writer App",
-          projectDescription: "A ReactJS-based tool for real-time Markdown editing and previewing. With a split-screen layout and user-friendly interface, effortlessly create and format Markdown content. ",
-          projectImgUrl: "https://pbs.twimg.com/media/GHhjZhrbkAMCJ0o?format=jpg&name=large",
-          projectUrl: "https://eznote.vercel.app/"
-        },
-        {
-          projectName: "Paytm Clone (Payment App)",
-          projectDescription: "The Payment App is a full-stack web application which demonstrate streamline user registration, authentication, and money transfer processes. Built with a Node.js backend using Express, MongoDB for data storage, and a React frontend, the app provides a secure and efficient platform for financial transactions.",
-          projectImgUrl: "https://pbs.twimg.com/media/GEtRaK6akAAHZ54?format=png&name=large",
-          projectUrl: "https://github.com/tsahil01/Paytm-Clone"
-        },
-        {
-          projectName: "Async-Await Simplified: A JavaScript MongoDB Example",
-          projectDescription: "My First Blog on Medium, explaining Async-Await in Javascript using a MongoDB example",
-          projectImgUrl: "",
-          projectUrl: "https://medium.com/@sahiltiwaskar2003/async-await-simplified-a-javascript-mongodb-example-b802abadb1a8"
-        }
-      ]
+  key: "projectsAtom",
+  default: [
+    {
+      projectName:
+        "Web-Dock PlayGround: Spin Up Your Favourite Development Environment",
+      projectDescription:
+        "A SaaS platform enabling instant access to Ubuntu Docker containers with pre-installed tools and languages, including VSCode, all from the browser, with automatic disposal after use.",
+      projectImgUrl: "/webdock.png",
+      projectUrl: "https://github.com/tsahil01/containers-web-editor",
+    },
+    {
+      projectName: "QuickShare: Effortlessly Share Your Files Anywhere",
+      projectDescription:
+        "Simplify your file sharing experience with our intuitive platform. Seamlessly upload, share, and access your files anytime, anywhere. Start sharing smarter today!",
+      projectImgUrl: "/quickShare.png",
+      projectUrl: "https://ezfiles.vercel.app/",
+    },
+    {
+      projectName: "Eznote: Markdown Writer App",
+      projectDescription:
+        "A ReactJS-based tool for real-time Markdown editing and previewing. With a split-screen layout and user-friendly interface, effortlessly create and format Markdown content. ",
+      projectImgUrl:
+        "https://pbs.twimg.com/media/GHhjZhrbkAMCJ0o?format=jpg&name=large",
+      projectUrl: "https://eznote.vercel.app/",
+    },
+    {
+      projectName: "Paytm Clone (Payment App)",
+      projectDescription:
+        "The Payment App is a full-stack web application which demonstrate streamline user registration, authentication, and money transfer processes. Built with a Node.js backend using Express, MongoDB for data storage, and a React frontend, the app provides a secure and efficient platform for financial transactions.",
+      projectImgUrl:
+        "https://pbs.twimg.com/media/GEtRaK6akAAHZ54?format=png&name=large",
+      projectUrl: "https://github.com/tsahil01/Paytm-Clone",
+    },
+    {
+      projectName: "Async-Await Simplified: A JavaScript MongoDB Example",
+      projectDescription:
+        "My First Blog on Medium, explaining Async-Await in Javascript using a MongoDB example",
+      projectImgUrl: "",
+      projectUrl:
+        "https://medium.com/@sahiltiwaskar2003/async-await-simplified-a-javascript-mongodb-example-b802abadb1a8",
+    },
+  ],
 });
--- portfolio-frontend/src/components/ProjectsHeader.tsx
+++ portfolio-frontend/src/components/ProjectsHeader.tsx
@@ -1,7 +1,16 @@
-export function ProjectsHeader(){
-    return <>
-    <h1 className="text-white md:text-5xl text-4xl font-bold mx-auto lg:w-3/5 w-full">
-    <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-indigo-400">Projects</span> that I have <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-blue-400">Build</span> so far.
-    </h1>
+export function ProjectsHeader() {
+  return (
+    <>
+      <h1 className="text-white md:text-5xl text-4xl font-bold mx-auto lg:w-3/5 w-full">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-indigo-400">
+          Projects
+        </span>{" "}
+        that I have{" "}
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-blue-400">
+          Build
+        </span>{" "}
+        so far!
+      </h1>
     </>
+  );
 }
